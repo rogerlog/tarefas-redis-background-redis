a4f7451d169866cbc03d4dd0a26ee02df3cb5776
Roger <logroger@gmail.com>
2021-02-07 19:51:05 -0300

    Add server

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on the ${process.env.PORT}`)
+});
